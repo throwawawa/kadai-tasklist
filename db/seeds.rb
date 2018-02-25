1f9c79baaf345de2487dc0d3617d29535921e1ee
kazu.awano <thadmadm@gmail.com>
2018-02-25 11:07:10 +0000

    add pagenation testdata

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+status = ['未着手', '進行中', '確認待ち', '完了済']
+(1..100).each do |number|
+  Task.create(status: status.sample, content: 'タスク' + number.to_s)
+end
